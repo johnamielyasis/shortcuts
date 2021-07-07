84a91584acef64428acc42fea8e93c7d5ebd7dd6
John Yasis <johnamielyasis@gmail.com>
2021-07-07 15:40:43 -0700

    first shortcut completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { HotKeys } from "react-hotkeys";
+import React, { useState } from 'react';
 
-function App() {
+// create array of shortcut objects
+const shortcuts = [
+  {
+    name: "Duplicate Line Down",
+    description: "Make a copy of current line that cursor is on below",
+    image: "dupe.gif",
+    keystroke: "shift+option+down"
+  },
+  {
+    name: "Duplicate Line Up",
+    description: "Make a copy of current line that cursor is on up",
+    image: "dup.gif",
+    keystroke: "shift+option+up"
+  }
+]
+
+const App = () => {
+  const [current, setCurrent] = useState(0);
+
+  const currentShortcut = shortcuts[current];
+  const handleSuccess = React.useCallback(() => {
+    // logic here
+    setCurrent(current + 1);
+  }, [])
+
+  const handlers = {
+    SUCCESS: handleSuccess
+  };
+
+  const keyMap = {
+    SUCCESS: currentShortcut.keystroke
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HotKeys keyMap={keyMap} handlers={handlers} >
+      <div>
+        {currentShortcut.name}
+      </div>
+    </HotKeys>
   );
-}
+};
 
 export default App;
