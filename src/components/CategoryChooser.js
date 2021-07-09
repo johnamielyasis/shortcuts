e8840bd2334aa688aeb7f6285585e5581b848884
John Yasis <johnamielyasis@gmail.com>
2021-07-09 11:21:43 -0700

    reversion to old not broken state

--- src/components/CategoryChooser.js
+++ src/components/CategoryChooser.js
@@ -8,20 +8,9 @@ import styled from 'styled-components';
   useSetRecoilState = (atom) => setterFn          // only returns setterFn ONLY
 */
 const CategoryButton = styled.button`
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   padding: 16px;
   margin-right: 16px;
-=======
   padding: 8px;
->>>>>>> parent of 6659166 (added score, now properly updating progress in db, style header, some refactor)
-=======
-  padding: 8px;
->>>>>>> parent of 6659166 (added score, now properly updating progress in db, style header, some refactor)
-=======
-  padding: 8px;
->>>>>>> parent of 6659166 (added score, now properly updating progress in db, style header, some refactor)
 `;
 
 export default function CategoryChooser(props) {
